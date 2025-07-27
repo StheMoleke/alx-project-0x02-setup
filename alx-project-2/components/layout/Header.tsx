c32dc08d0167454de2e15a417ab33eb770a06cbf
Sthembiso Moleke <stherabite9@gmail.com>
2025-07-27 21:50:03 +0200

    Update Header.tsx

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+// components/layout/Header.tsx
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to My Next.js App</h1>
+    </header>
+  );
+};
+
+export default Header;
