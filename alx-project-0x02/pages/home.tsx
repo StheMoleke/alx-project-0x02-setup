e1887e599d1b6c98704e42e27bb16a6f0430bef3
sthembiso <stherabite9@gmail.com>
2025-07-28 05:39:12 +0200

    Create reusable Card component and use it in Home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,12 +1,20 @@
-// pages/home.tsx
-
 import React from "react";
+import Card from "../components/common/Card";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of the project.</p>
+
+      <Card
+        title="Card 1"
+        content="This is the content of the first card."
+      />
+
+      <Card
+        title="Card 2"
+        content="This is some different content for the second card."
+      />
     </div>
   );
 };
